167bc51cacf1d8f43cbd3604d575999f204f82b4
Veerabomma Abinav <abhiveerabomma@gmail.com>
2025-11-12 19:35:56 +0530

    Rename route functions and templates to title case

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import flask,render_template
 app=flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def Home():
+    return render_template("Home.html")
 
 @app.route("/")
-def menu():
-    return render_template("menu.html")
+def Menu():
+    return render_template("Menu.html")
 
 @app.route("/")
-def contact():
-    return render_template("contact.html")
+def Contact():
+    return render_template("Contact.html")
 
 if __name__=="__main__":
     app.run(debug=True)
